89774bec16f5f2a2b8517642ca64643e0af0401f
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2023-11-29 20:09:29 +0000

    add typewriter function for style and add story content

--- README.md
+++ README.md
@@ -29,3 +29,6 @@ The deployment terminal is set to 80 columns by 24 rows. That means that each li
 
 -----
 Happy coding!
+
+# Acknowledgements
+https://stackoverflow.com/questions/20302331/typing-effect-in-python
--- run.py
+++ run.py
@@ -4,7 +4,16 @@
 import colorama
 from colorama import Fore
 from art import *
+import sys
+import time
 colorama.init(autoreset=True)
+
+
+def typewriter(string):
+    for i in string:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(0.05)
 
 
 def start_game():
@@ -12,8 +21,17 @@ def start_game():
     Initialise a new game
     """
     # print(f"{Fore.Green}{opening_logo}")
-    print(f"{Fore.GREEN}{opening_logo}")
-    data = input("Would you like to play: Y/N \n")
-    return data
+    print(f"{Fore.YELLOW}{opening_logo}")
+    typewriter('Welcome!\n')
+    typewriter('You are about to take on the role of Guybrush Threepwood\n')
+    typewriter('The villainous ghost pirate LeChuck has kidnapped Governor Elaine Marley\n')
+    typewriter('You have procured your own pirate ship and assembled a crew\n')
+    typewriter('You must now as pursue LeChuck back to his hideout on Monkey Island\n')
+    user_state = input("Are you ready to begin the adventure: Y/N \n").lower()
+    return user_state
+
+
 
 start_game()
+
+
